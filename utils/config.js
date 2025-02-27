a2c5cbebae750fb87dfeb0ba66af82244947ffe4
Dejavu <32336181+yndra04@users.noreply.github.com>
2025-02-28 06:38:40 +0700

    config.js

---
+++ utils/config.js
@@ -0,0 +1,48 @@
+export const CONTRACT_ADDRESS = "0x7c49EF7bf7d8C7Af63C7DFB0A43b3640B8098607";
+
+export const CONTRACT_ABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "click",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      }
+    ],
+    "name": "getClicks",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
