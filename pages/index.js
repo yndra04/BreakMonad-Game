2b0055cc5b096acd47c8cb9e8c8820bf6137b105
Dejavu <32336181+yndra04@users.noreply.github.com>
2025-02-28 06:39:48 +0700

    index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import ClickGame from "../components/ClickGame";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <ClickGame />
+    </div>
+  );
+}
