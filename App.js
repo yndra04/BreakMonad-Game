2831accf1cfe1ff7a9dc965f5eeef482d97dd1df
Dejavu <32336181+yndra04@users.noreply.github.com>
2025-02-28 06:40:41 +0700

    App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import ClickGame from "./components/ClickGame";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <ClickGame />
+    </div>
+  );
+}
+
+export default App;
